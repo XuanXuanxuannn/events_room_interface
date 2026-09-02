869ab51220f7c0d465644563cd02efb1251174ae
Xuan Liu <liu987654321xuan@gmail.com>
2026-09-02 22:59:56 +1000

    repo cleanup

--- backend/test.py
+++
@@ -1,9 +0,0 @@
-from models import SessionLocal, ContentItem
-
-db = SessionLocal()
-items = db.query(ContentItem).all()
-
-for item in items:
-    print(item.id, item.title, item.type)
-
-db.close()
